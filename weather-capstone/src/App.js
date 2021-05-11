1300c4fd65272fa6c9a119bdf0beac94e0aa4090
Ethan Vesely <Evesel259@wes-mec.org>
2021-05-11 16:32:06 -0700

    added the AboutUs Component

--- weather-capstone/src/App.js
+++ weather-capstone/src/App.js
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar'
+import AboutUs from './components/AboutUs'
 
 function App() {
   return (
---
+++ weather-capstone/src/components/AboutUs.js
@@ -0,0 +1,9 @@
+const AboutUs = () => {
+    return (
+        <div>
+            <h4>Arizona Weather</h4>
+        </div>
+    )
+}
+
+export default AboutUs
