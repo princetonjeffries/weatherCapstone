4eb41e449d9aa02601fafb2a2670d914e0394fbc
Ethan Vesely <Evesel259@west-mec.org>
2021-05-12 15:19:17 -0700

    worked on the carousel for the about page and moved some files around

--- weather-capstone/src/App.js
+++ weather-capstone/src/App.js
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar/Navbar'
 import {BrowserRouter as Router} from 'react-router-dom'
-import AboutUs from './components/pages/AboutUs'
+import AboutUs from './components/pages/AboutUs/AboutUs'
 function App() {
   return (
     <Router>
