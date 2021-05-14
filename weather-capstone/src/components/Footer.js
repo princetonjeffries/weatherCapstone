d8b343ec88b972673723145ded41f02d91ad9569
princetonjeffries <71167628+princetonjeffries@users.noreply.github.com>
2021-05-13 17:29:16 -0700

    logos added

--- weather-capstone/src/components/Footer.js
+++ weather-capstone/src/components/Footer.js
@@ -11,15 +11,15 @@ const Footer = () => {
             </Card.Header>
             <div id='restOfFooter' className='text-center'>
              <WeatherSunnyLow size='100'/>
-                <ul>
-                    <li>Twitter</li>
-                    <li>Instagram</li>
-                    <li>Facebook</li>
+                <ul id='links1'>
+                    <li><a href='#' ><img src='https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png' height='64' width='64'></img> </a></li>
+                    <li><a href='#'><img src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png' height='64' width='64'></img></a></li>
+                    <li><a href='#'><img src='https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png' height='64' width='64'></img></a></li>
                 </ul>
-                <ul>
-                    <li>Contact Us</li>
-                    <li>Leave a Review</li>
-                    <li>Buy Merchandise</li>
+                <ul id='links'>
+                    <li><a href='#'>Contact Us</a></li>
+                    <li><a href='#'>Leave a Review</a></li>
+                    <li><a href='#'>Buy Merchandise</a></li>
                 </ul>
             </div>
                 <p className='text-center'>Copyright @ 2021</p>
