0fb034ee73e9b751c1c7ed5b7777bff3cc1ca126
Ethan Vesely <Evesel259@wes-mec.org>
2021-05-17 18:19:01 -0700

    added a carousel to the AboutUs page and updated some styles

--- weather-capstone/src/components/pages/AboutUs/Cards.js
+++ weather-capstone/src/components/pages/AboutUs/Cards.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Carousel from 'react-bootstrap/Carousel'
 
 /* This Sun icon was imported from Styled Icons */
 import {WeatherSunnyLow} from '@styled-icons/fluentui-system-filled/WeatherSunnyLow';
@@ -15,10 +16,19 @@ import image2 from './img/image2.jpg';
 import image3 from './img/image3.jpg';
 import image4 from './img/image4.jpg';
 import image5 from './img/image5.jpg';
+import image6 from './img/image6.png';
+import image7 from './img/image7.jpg';
+import image8 from './img/image8.jpg';
+import image9 from './img/image9.jpg';
 
 const Cards = () => { /* This is the cards function */
   return (
   <>
+
+  <Carousel interval="100000">
+
+  <Carousel.Item>
+
   <Jumbotron id="jumbotron-2"> {/* Jumbotron is a React Bootstrap Component */}
 
     <div className="header">
@@ -27,7 +37,7 @@ const Cards = () => { /* This is the cards function */
 
   <CardDeck> {/* All of the cards are inside a card deck to align them evenly */}
 
-  <Card> {/* Each set of information and images are held in cards */}
+  <Card className="custom-card"> {/* Each set of information and images are held in cards */}
     <Card.Body> {/* Card Bodies put padding around the content inside the card */}
       <Card.Title id="title">Our Mission</Card.Title> {/* Titles have a larger font-size compared to the rest of the text and are at the top */}
       <Card.Text id="text"> {/* Card text holds the rest of the information */}
@@ -40,7 +50,7 @@ const Cards = () => { /* This is the cards function */
     </Card.Body>
   </Card>
 
-  <Card> {/* Each set of information and images are held in cards */}
+  <Card className="custom-card"> {/* Each set of information and images are held in cards */}
     <Card.Body> {/* Card Bodies put padding around the content inside the card */}
       <Card.Title id="title">Our Vision</Card.Title> {/* Titles have a larger font-size compared to the rest of the text and are at the top */}
       <Card.Text id="text"> {/* Card text holds the rest of the information */}
@@ -53,7 +63,7 @@ const Cards = () => { /* This is the cards function */
     </Card.Body>
   </Card>
 
-  <Card> {/* Each set of information and images are held in cards */}
+  <Card className="custom-card"> {/* Each set of information and images are held in cards */}
     <Card.Body> {/* Card Bodies put padding around the content inside the card */}
       <Card.Title id="title">Our Goals</Card.Title> {/* Titles have a larger font-size compared to the rest of the text and are at the top */}
       <Card.Text id="text"> {/* Card text holds the rest of the information */}
@@ -72,6 +82,80 @@ const Cards = () => { /* This is the cards function */
 </CardDeck>
 
 </Jumbotron>
+
+</Carousel.Item>
+
+
+<Carousel.Item>
+
+  <Jumbotron id="jumbotron-2"> {/* Jumbotron is a React Bootstrap Component */}
+
+  <div className="header">
+    <h1 id="heading">How we retrieve data</h1> {/* The Sun icon from Styled Icons is used here */}
+  </div>
+
+  <CardDeck> {/* All of the cards are inside a card deck to align them evenly */}
+
+  <Card className="custom-card"> {/* Each set of information and images are held in cards */}
+    <Card.Body> {/* Card Bodies put padding around the content inside the card */}
+      <Card.Title id="title">Our Methods</Card.Title> {/* Titles have a larger font-size compared to the rest of the text and are at the top */}
+      <Card.Text id="text"> {/* Card text holds the rest of the information */}
+        We use the newest and most accurate methods to ensure accurate forecasts.
+      </Card.Text>
+      <Card.Img variant="top" id="methods-image" src={image6} className="custom-image"/> {/* Card Images display images inside cards */}
+        <Card.Text id="text">The exact methods that we use for our forecasts are Deterministic forecasting and Probabilistic forecasting.</Card.Text>
+    </Card.Body>
+  </Card>
+
+  <Card className="custom-card"> {/* Each set of information and images are held in cards */}
+    <Card.Body> {/* Card Bodies put padding around the content inside the card */}
+      <Card.Title id="title">Our Tools</Card.Title> {/* Titles have a larger font-size compared to the rest of the text and are at the top */}
+      <Card.Text id="text"> {/* Card text holds the rest of the information */}
+        We are using new technology like portable dust generators and Doppler Radars.
+      </Card.Text>
+      <Card.Img variant="top" src={image7} className="custom-image"/> {/* Card Images display images inside cards */}
+      <Card.Body>
+      <Card.Img variant="top" src={image8} className="custom-image"/>
+      </Card.Body>
+    </Card.Body>
+  </Card>
+
+  <Card className="custom-card"> {/* Each set of information and images are held in cards */}
+    <Card.Body> {/* Card Bodies put padding around the content inside the card */}
+      <Card.Title id="title">Our Ideas</Card.Title> {/* Titles have a larger font-size compared to the rest of the text and are at the top */}
+      <a href="https://www.barnesandnoble.com/w/reginald-sutcliffe-and-the-invention-of-modern-weather-systems-science-jonathan-e-martin/1137665805"><Card.Img variant="top" src={image9} className="custom-image"/></a> {/* Card Images display images inside cards */}
+    </Card.Body>
+  </Card>
+
+</CardDeck>
+
+</Jumbotron>
+
+</Carousel.Item>
+
+
+<Carousel.Item>
+
+  <Jumbotron id="jumbotron-2"> {/* Jumbotron is a React Bootstrap Component */}
+
+  <div className="header">
+    <h1 id="heading">Our Story</h1> {/* The Sun icon from Styled Icons is used here */}
+  </div>
+
+  <div className="scroll">
+  <h1 id="title-2"><strong>The History of Arizona Weather</strong></h1>
+  <h1 id="title-3"><strong>Introduction</strong></h1>
+  <p>Arizona Weather was founded on February 14<sup>th</sup>, 1949. It was founded by Abe Cleveland. Abe Cleaveland thought it was important to provide Arizonans with up to date weather reports to ensure everyone's safety. </p> 
+  
+  <h1 id="title-3"><strong>Part One - Loss</strong></h1>
+  <p>Abe was walking down the street on his way to his brother's wedding. He was the best man so he had to be on time and have the ring. He kept the ring in his right jacket pocket in a black wooden case. Every 5 minutes he would check to make sure he still had the ring in his pocket. After about his fifth check he saw a dust storm heading straight for him. He tried to hide behind a sign but it didn't work. His suit was covered in dirt. He didn't care too much but then he noticed that the ring in his pocket was missing.</p>
+</div>
+
+</Jumbotron>
+
+</Carousel.Item>
+
+</Carousel>
 </>
 
   )
