53827ec149ee038407c06f10c91acb60d7fb10b9
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2021-05-19 18:26:21 -0700

    commented stories.js, storyrow.js, and story.js

--- weather-capstone/src/components/pages/Stories/Story.js
+++ weather-capstone/src/components/pages/Stories/Story.js
@@ -1,16 +1,23 @@
 import Card from 'react-bootstrap/Card'
 import { Col } from 'react-bootstrap'
-import './css/Stories.css';
+//imported card and col from react bootstrap
 
 const Story = ({src, date, title, text}) => {
     return (
-        <Col style={{ marginTop: '15px', display: 'flex', justifyContent: 'center'}} md={4}>
-            <Card style={{ height: 'fit-content' }}>
+        <Col style={{ marginTop: '15px'}} md={4}>
+        {/* created a col component, with a margin top of 15px and a md size of 4 */}
+            <Card>
+            {/* card component */}
                 <Card.Img variant="top" src={src} />
+                {/* card image with variant set as top and src set a src prop */}
                 <Card.Body>
+                {/* card body component to content text information */}
                     <Card.Subtitle className="mb-2 text-muted">{date}</Card.Subtitle>
+                    {/* card subtitle with a className of 'mb-2 text-muted', and its texxt set as the date prop */}
                     <Card.Title>{title}</Card.Title>
+                    {/* card title with the text set as the title prop */}
                     <Card.Text>{text}</Card.Text>
+                    {/* card text with the text set as the text prop */}
                 </Card.Body>
             </Card>
         </Col>
--- weather-capstone/src/components/pages/Stories/StoryRow.js
+++ weather-capstone/src/components/pages/Stories/StoryRow.js
@@ -1,13 +1,17 @@
-import { Row, Col } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
 import Story from './Story'
+//Row imported from react bootstrap and story component imported
 
 const StoryRow = ({src1, date1, title1, text1, src2, date2, title2, text2, src3, date3, title3, text3}) => {
-
     return (
         <Row style={{marginTop: '15px', marginBottom: '15px'}}>
+        {/* row component with margin top of 15px and a margin bottom of 15px */}
             <Story src={src1} date={date1} title={title1} text={text1} />
+            {/* story component with date set as the date1 prop, title set as the title1 prop, and text set as the text1 prop */}
             <Story src={src2} date={date2} title={title2} text={text2} />
+            {/* story component with date set as the date2 prop, title set as the title2 prop, and text set as the text2prop */}
             <Story src={src3} date={date3} title={title3} text={text3} />
+            {/* story component with date set as the date3 prop, title set as the title3 prop, and text set as the text3 prop */}
         </Row>
     )
 }
